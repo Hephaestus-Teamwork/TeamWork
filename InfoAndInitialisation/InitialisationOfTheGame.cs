93a9cf0f92c5f74e172a0ca0a5f24e5d29806d1e
Todor Enikov <enigmaenikov@gmail.com>
2016-06-20 23:09:54 +0300

    Current version of the game!

---
+++ InfoAndInitialisation/InitialisationOfTheGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TeamWork
+{
+    public static class InitialisationOfTheGame
+    {
+        public static int playGroundWidth = 30;
+        public static long scores;
+        public static int livesCount = 3;
+
+        
+        public static void Initialisation()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
+            Console.Title = "Falling Rocks";
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth = 50;
+            scores = 0;        
+        }
+
+        public static void Sleep()
+        {
+            Thread.Sleep(200);
+        }
+    }
+}
