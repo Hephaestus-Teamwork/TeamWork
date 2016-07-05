2476d603326eebd1fa23c5d33475b0baf7d88c12
Todor Enikov <enigmaenikov@gmail.com>
2016-07-05 14:20:48 +0300

    Added folder and structure - StructureForPlayer.cs.

---
+++ Structure/StructureForPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork.Structure
+{
+    public struct StructureForPlayer
+    {
+    }
+}
