488cf598df00a119e8381a4b6dd33928917613f4
Todor Enikov <enigmaenikov@gmail.com>
2016-07-05 14:22:05 +0300

    Added fields, properties and constructor for the structure.

--- Structure/StructureForPlayer.cs
+++ Structure/StructureForPlayer.cs
@@ -8,5 +8,25 @@ namespace TeamWork.Structure
 {
     public struct StructureForPlayer
     {
+        private string body;
+        private int x;
+
+        public StructureForPlayer(string body, int x)
+            : this()
+        {
+            this.Body = body;
+            this.X = x;
+        }
+        public string Body
+        {
+            get { return this.body; }
+            set { this.body = value; }
+        }
+
+        public int X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
     }
 }
