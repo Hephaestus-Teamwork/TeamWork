ae49ecd04e328960fa123bb3ac59128bfd5bd23b
Y.Penev <dany.penev@gmail.com>
2016-07-05 16:56:35 +0300

    Adding new player layout

--- LettersAndPlayerClasses/Player.cs
+++ LettersAndPlayerClasses/Player.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TeamWork.Structure;
 
 namespace TeamWork
 {
     public class Player
     {
         private static Player instance = null;
-        StructureForPlayer player = new StructureForPlayer("(0)", InitialisationOfTheGame.playGroundWidth / 2 - 1);
+        private string body;
+        private int x; // position of '('
 
         private Player()
         {
+            body = "\\" + ((char)2).ToString() + "/";
+            x = InitialisationOfTheGame.playGroundWidth / 2 - 1;
             Print();
         }
         public static Player GetInstance()
@@ -26,29 +28,29 @@ namespace TeamWork
         }
         public void MoveLeft()
         {
-            if (this.player.X > 0)
+            if (x > 0)
             {
-                this.player.X--;
+                x--;
             }
         }
         public void MoveRight()
         {
-            if (this.player.X < InitialisationOfTheGame.playGroundWidth - 2)
+            if (x < InitialisationOfTheGame.playGroundWidth - 2)
             {
-                this.player.X++;
+                x++;
             }
         }
         public void Print(bool hit = false)
         {
-            Console.SetCursorPosition(this.player.X, Console.WindowHeight - 1);
-            Console.ForegroundColor = hit ? ConsoleColor.Red : ConsoleColor.White;
-            Console.Write(hit ? "XXX" : this.player.Body);
+            Console.SetCursorPosition(x, Console.WindowHeight - 1);
+            Console.ForegroundColor = hit ? ConsoleColor.Red : ConsoleColor.Yellow;
+            Console.Write(hit ? "XXX" : body);
         }
         public bool Overlap(Letters r)
         {
             if (Console.WindowHeight - 1 == r.Y)
             {
-                if (this.player.X == r.X || this.player.X + 1 == r.X || this.player.X + 1 == r.X)
+                if (x == r.X || x + 1 == r.X || x + 2 == r.X)
                 {
                     return true;
                 }
