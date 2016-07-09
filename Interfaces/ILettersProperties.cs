a5a854c04d7c37af53aa06e0512724e47ae99ff0
Todor Enikov <enigmaenikov@gmail.com>
2016-07-09 21:10:55 +0300

    Created interface ILettersProperties.

---
+++ Interfaces/ILettersProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork.Interfaces
+{
+    public interface ILettersProperties
+    {
+        int X { get; }
+        int Y { get; }
+        ConsoleColor Colour { get; }
+        EnumerationOfCharacters RandomEnumeration { get; }
+    }
+}
