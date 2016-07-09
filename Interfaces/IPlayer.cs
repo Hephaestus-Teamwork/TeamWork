fbce053c04df3a7bb11b4a76e52c16441047c08e
Todor Enikov <enigmaenikov@gmail.com>
2016-07-09 21:09:35 +0300

    Created interface IPlayer.

---
+++ Interfaces/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork.Interfaces
+{
+    public interface IPlayer
+    {
+        void MoveLeft();
+        void MoveRight();
+    }
+}
