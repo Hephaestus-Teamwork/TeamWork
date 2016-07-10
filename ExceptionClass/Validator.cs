582619a092a61cd02fc11de80a421844295b28f2
Todor Enikov <enigmaenikov@gmail.com>
2016-07-10 12:10:03 +0300

    Added method CheckIfNull in class Validator.cs.

--- ExceptionClass/Validator.cs
+++ ExceptionClass/Validator.cs
@@ -8,6 +8,12 @@ namespace TeamWork.ExceptionClass
 {
     public static class Validator
     {
-       
+        public static void CheckIfNull(object obj, string message = null)
+        {
+            if (obj==null)
+            {
+                throw new NullReferenceException(message);
+            }
+        }
     }
 }
