eb4c9f7e45682f2304dd64b23d710299ce9d1958
Todor Enikov <enigmaenikov@gmail.com>
2016-07-11 23:37:51 +0300

    Added using for MenuForTheGame and used the method from this class to create menu.

--- StartTheGame.cs
+++ StartTheGame.cs
@@ -6,96 +6,109 @@ namespace TeamWork
 {
     public class StartTheGame
     {
+
         static Random rand = new Random();
         static List<Letters> letters;
         static void Main()
         {
-            InitialisationOfTheGame.Initialisation();
-            letters = new List<Letters>();
-            Player player = Player.GetInstance();
-            List<Letters> removeObsoletes;
-            bool hit = false;
-            ConsoleKeyInfo pressedKey;
-            while (true)
+            string pressedInitialKey = MenuForTheGame.PressingKey();
+            if (pressedInitialKey == "Start")
             {
-                // adding new letters
-                for (int i = 0, count = rand.Next(InitialisationOfTheGame.playGroundWidth / (InitialisationOfTheGame.scores > 100 ? 7 : 10)); i < count; i++)
+                InitialisationOfTheGame.Initialisation();
+                letters = new List<Letters>();
+                Player player = Player.GetInstance();
+                List<Letters> removeObsoletes;
+                bool hit = false;
+                ConsoleKeyInfo pressedKey;
+                while (true)
                 {
-                    letters.Add(new Letters());
-                }
-                // moving our player
-                while (Console.KeyAvailable)
-                {
-                    pressedKey = Console.ReadKey(true);
-                    if (pressedKey.Key == ConsoleKey.LeftArrow || pressedKey.Key == ConsoleKey.A) // <--
+                    // adding new letters
+                    for (int i = 0, count = rand.Next(InitialisationOfTheGame.playGroundWidth / (InitialisationOfTheGame.scores > 100 ? 7 : 10)); i < count; i++)
                     {
-                        player.MoveLeft();
+                        letters.Add(new Letters());
                     }
-                    if (pressedKey.Key == ConsoleKey.RightArrow || pressedKey.Key == ConsoleKey.D) // -->
+                    // moving our player
+                    while (Console.KeyAvailable)
                     {
-                        player.MoveRight();
-                    }
-                    if (pressedKey.Key == ConsoleKey.Escape) // Esc
-                    {
-                        Environment.Exit(0); // leave game
-                    }
-                }
-                removeObsoletes = new List<Letters>(); // letters, which are to leave the screen
-                hit = false;
-                foreach (Letters letter in letters)
-                {
-                    if (!letter.MoveDown())
-                    {
-                        removeObsoletes.Add(letter);
-                    }
-                    if (player.Overlap(letter)) // if hit
-                    {
-                        Hit.FlashScreen();
-                        hit = true;
-
-                        if (!Hit.Questions(letter))
+                        pressedKey = Console.ReadKey(true);
+                        if (pressedKey.Key == ConsoleKey.LeftArrow || pressedKey.Key == ConsoleKey.A) // <--
                         {
-                            InitialisationOfTheGame.livesCount--;
-                          
-                            if (InitialisationOfTheGame.livesCount == 0) // game over 
+                            player.MoveLeft();
+                        }
+                        if (pressedKey.Key == ConsoleKey.RightArrow || pressedKey.Key == ConsoleKey.D) // -->
+                        {
+                            player.MoveRight();
+                        }
+                        if (pressedKey.Key == ConsoleKey.Escape) // Esc
+                        {
+                            Environment.Exit(0); // leave game
+                        }
+                    }
+                    removeObsoletes = new List<Letters>(); // letters, which are to leave the screen
+                    hit = false;
+                    foreach (Letters letter in letters)
+                    {
+                        if (!letter.MoveDown())
+                        {
+                            removeObsoletes.Add(letter);
+                        }
+                        if (player.Overlap(letter)) // if hit
+                        {
+                            Hit.FlashScreen();
+                            hit = true;
+
+                            if (!Hit.Questions(letter))
                             {
-                                Console.SetCursorPosition(14, 8);
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("GAME OVER!!!");
+                                InitialisationOfTheGame.livesCount--;
 
-                                Console.SetCursorPosition(17, 10);
-                                Console.Out.WriteLine(((char)3).ToString() + " " + ((char)3).ToString() + " " + ((char)3).ToString()); //Three hearts
+                                if (InitialisationOfTheGame.livesCount == 0) // game over 
+                                {
+                                    Console.SetCursorPosition(14, 8);
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("GAME OVER!!!");
 
-                                Console.SetCursorPosition(12, 12);
-                                Console.Out.WriteLine("Your score is {0} ", InitialisationOfTheGame.scores); //Result
+                                    Console.SetCursorPosition(17, 10);
+                                    Console.Out.WriteLine(((char)3).ToString() + " " + ((char)3).ToString() + " " + ((char)3).ToString()); //Three hearts
 
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.SetCursorPosition(0, Console.WindowHeight - 1);
-                                Environment.Exit(0);
+                                    Console.SetCursorPosition(12, 12);
+                                    Console.Out.WriteLine("Your score is {0} ", InitialisationOfTheGame.scores); //Result
+
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                                    Environment.Exit(0);
+                                }
                             }
                         }
                     }
-                }
 
-                Console.Clear();
-                foreach (Letters old in removeObsoletes) // removing letters, which are to leave the screen
-                {
-                    letters.Remove(old);
-                    InitialisationOfTheGame.scores++;
+                    Console.Clear();
+                    foreach (Letters old in removeObsoletes) // removing letters, which are to leave the screen
+                    {
+                        letters.Remove(old);
+                        InitialisationOfTheGame.scores++;
+                    }
+                    if (hit)
+                    {
+                        letters.Clear();
+                        InitialisationOfTheGame.scores -= 10;
+                    }
+                    // printing
+                    foreach (Letters letter in letters)
+                    {
+                        letter.Print();
+                    }
+                    player.Print(hit);
+                    Info.DisplayInfo();
+                    InitialisationOfTheGame.Sleep();
                 }
-                if (hit)
-                {
-                    letters.Clear();
-                    InitialisationOfTheGame.scores -= 10;
-                }
-                // printing
-                foreach (Letters letter in letters)
-                {
-                    letter.Print();
-                }
-                player.Print(hit);
-                Info.DisplayInfo();
-                InitialisationOfTheGame.Sleep();
+            }
+            else if (pressedInitialKey == "High")
+            {
+
+            }
+            else if (pressedInitialKey == "Exit")
+            {
+                return;
             }
         }
     }
