86c4071e22c70c22e641c07196d8b26e34d664d8
Y.Penev <dany.penev@gmail.com>
2016-07-12 11:51:15 +0300

    Added working question screen

--- InfoAndInitialisation/Hit.cs
+++ InfoAndInitialisation/Hit.cs
@@ -29,31 +29,28 @@ namespace TeamWork.InfoAndInitialisation
 
         public static bool Questions(Letters letter)
         {
-            bool isAnswerCorrect = false;
             Random rand = new Random();
-            int randomNumber = rand.Next(0, 5);
+            int number = rand.Next(0, 5);
 
             Console.Clear();
-            try
-            {
-                string s = "D:/Study/_TELERIK/TeamWork/Questions/"; // + letter + randomNumber;
-                StreamReader sr = new StreamReader(@s, Encoding.Default);
-                int count = File.ReadAllLines(@s).Length / 2;
-                Console.WriteLine("---Explain the rules---");
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
 
-                //Print hole file on the screeen
+            string s = @"D:\Study\_TELERIK\TeamWork\TeamWork-master\TeamWork-master\TeamWork\Questions\Algebra\1.txt"; // + letter + randomNumber;
+            StreamReader sr = new StreamReader(@s, Encoding.Default);
+            var text = File.ReadAllLines(@s);
 
-              //  TODO add letter in main program constructor
-            }
-            catch (Exception)
+            foreach (var line in text)
             {
-                Console.WriteLine("Error!");
-                hitLetter = letter.RandomEnumeration.ToString();
+                Console.WriteLine(line);
             }
 
-            if (isAnswerCorrect)
+            string pressedKey = Console.ReadLine();
+
+
+            //Print hole file on the screeen
+            //  TODO add letter in main program constructor
+
+
+            if (int.Parse(pressedKey) == number)
             {
                 Console.WriteLine("You pass !");
                 Console.ReadKey();
